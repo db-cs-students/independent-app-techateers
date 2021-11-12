e822e6c8deaa973e26131d94f122d4edff73fb21
Sam <dogbat4321@gmail.com>
2021-11-12 12:15:01 -0500

    Initial commit

---
+++ Teacher App/Teacher_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Teacher_AppApp.swift
+//  Teacher App
+//
+//  Created by Sam L on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Teacher_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
