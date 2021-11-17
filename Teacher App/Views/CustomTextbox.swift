61468e0fac33e1bf12f13635bf4d06f1620b6647
Cameron M <cameronmoore3131@gmail.com>
2021-11-17 12:33:24 -0500

    add customTextbox file

---
+++ Teacher App/Views/CustomTextbox.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextbox.swift
+//  Teacher App
+//
+//  Created by Cameron M on 11/17/21.
+//
+
+import SwiftUI
+
+struct CustomTextbox: View {
+    var body: some View {
+        Text("Hello Cameron")
+    }
+}
+
+struct CustomTextbox_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTextbox()
+    }
+}
