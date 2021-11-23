7b81605c2ee4beb1c2cb4b7f9d16006cf1167e11
Sam <dogbat4321@gmail.com>
2021-11-23 11:50:58 -0500

    FInished the basic layout of the leaderboard

--- Teacher App/Views/Leaderboard.swift
+++ Teacher App/Views/Leaderboard.swift
@@ -10,24 +10,40 @@ import SwiftUI
 
 
 struct CustomLeaderboard: View {
-    @State var Cname:String
-    @State var Studentlist: [String]
+    @State var cname:String
+    @State var studentlist: [String]
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(Studentlist , id: \.self) { student in
-                    HStack{
-                        Text(student)
+        VStack {
+            Text(cname)
+                .bold()
+                .foregroundColor(.blue)
+            ScrollView {
+                VStack (spacing: 30) {
+                    ForEach(studentlist , id: \.self) { student in
+                        ZStack (alignment: .leading) {
+                            Rectangle()
+                                .foregroundColor(.blue)
+                                .cornerRadius(10)
+                            HStack {
+                                Text("i \(student)")
+                                Spacer()
+                                Text("2000")
+                            }
+                            .padding()
+                            
+                        }
+                        
+                        .frame(width: 250, height: 40, alignment: .center)
+                        
                     }
                 }
             }
-            .navigationTitle(Cname)
         }
     }
 }
 
 struct CustomLeaderboard_Previews: PreviewProvider {
     static var previews: some View {
-        CustomLeaderboard(Cname:"A Block", Studentlist: ["Jimmy","John","Jake","Jason","cameron"])
+        CustomLeaderboard(cname:"A Block", studentlist: ["Jimmy","John","Jake","Jason","Cameron","John","Jake","Jason","Cameron","John","Jake","Jason","Cameron","John","Jake","Jason","Cameron"])
     }
 }
