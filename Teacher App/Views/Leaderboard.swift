474da7a65ba164fc9bd5a29ad7d682bc6d2ef006
Sam <dogbat4321@gmail.com>
2021-11-29 12:03:52 -0500

    Completed the leaderboard

--- Teacher App/Views/Leaderboard.swift
+++ Teacher App/Views/Leaderboard.swift
@@ -13,16 +13,10 @@ struct Student: Identifiable {
     var highScore: Int
 }
 
-var students = [
-    Student(name: "Bob", highScore: 2000),
-    Student(name: "Bailey", highScore: 100000000000000),
-    Student(name: "Sam", highScore: 1)
-].sorted(by: {$0.highScore > $1.highScore})
-
 
 struct CustomLeaderboard: View {
     @State var cname:String
-    @State var studentlist: [String: Int] = ["bob": 2]
+    var studentlist: [Student]
     var body: some View {
         VStack {
             Text(cname)
@@ -30,13 +24,12 @@ struct CustomLeaderboard: View {
                 .foregroundColor(.blue)
             ScrollView {
                 VStack (spacing: 30) {
-                    ForEach(students) { student in
+                    ForEach(studentlist) { student in
                         ZStack (alignment: .leading) {
                             Rectangle()
                                 .foregroundColor(.blue)
                                 .cornerRadius(10)
                             HStack {
-                                Text("\(students.where)")
                                 Text("\(student.name)")
                                 Spacer()
                                 Text("\(student.highScore)")
@@ -46,30 +39,21 @@ struct CustomLeaderboard: View {
                         
                         .frame(width: 250, height: 40, alignment: .center)
                     }
-//                    ForEach(studentlist , id: \.self) { student in
-//                        ZStack (alignment: .leading) {
-//                            Rectangle()
-//                                .foregroundColor(.blue)
-//                                .cornerRadius(10)
-//                            HStack {
-//                                Text(" \(student)")
-//                                Spacer()
-//                                Text("\(studentlist[student]!)")
-//                            }
-//                            .padding()
-//                        }
-//
-//                        .frame(width: 250, height: 40, alignment: .center)
-//
-//                    }
                 }
             }
         }
     }
 }
 
+
 struct CustomLeaderboard_Previews: PreviewProvider {
+    static let students: [Student] = [
+        Student(name: "Bob", highScore: 2000),
+        Student(name: "Bailey", highScore: 100000000000000),
+        Student(name: "Sam", highScore: 1)
+    ].sorted(by: {$0.highScore > $1.highScore})
+    
     static var previews: some View {
-        CustomLeaderboard(cname:"A Block", studentlist:  ["Jimmy": 100,"john": 200])
+        CustomLeaderboard(cname:"A Block", studentlist: students)
     }
 }
