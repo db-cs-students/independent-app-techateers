b0a688691b89ff5df661d42051f5cb8f8b054aed
Sam <dogbat4321@gmail.com>
2021-12-01 12:44:53 -0500

    Worked more on home view

--- Teacher App/Views/HomeScreenView.swift
+++ Teacher App/Views/HomeScreenView.swift
@@ -9,12 +9,37 @@ import SwiftUI
 
 struct HomeScreenView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack{
+                ScrollView(.horizontal){
+                    HStack(spacing: 10) {
+                        CustomButton(name: "linear equations", width: 150, height: 100, img: "gamecontroller")
+                        CustomButton(name: "linear equations", width: 150, height: 100, img: "gamecontroller")
+                        CustomButton(name: "linear equations", width: 150, height: 100, img: "gamecontroller")
+                        CustomButton(name: "linear equations", width: 150, height: 100, img: "gamecontroller")
+                        CustomButton(name: "linear equations", width: 150, height: 100, img: "gamecontroller")
+                    }
+                }
+                .padding()
+                ScrollView(.horizontal){
+                    HStack(spacing: 15) {
+                        CustomButton(name: "linear equations", width: 150, height: 100, img: "studentdesk")
+                        CustomButton(name: "linear equations", width: 150, height: 100, img: "studentdesk")
+                        CustomButton(name: "linear equations", width: 150, height: 100, img: "studentdesk")
+                        CustomButton(name: "linear equations", width: 150, height: 100, img: "studentdesk")
+                        CustomButton(name: "linear equations", width: 150, height: 100, img: "studentdesk")
+                    }
+                }
+                .padding()
+                Spacer()
+            }
+            
+        }
     }
 }
-
 struct HomeScreenView_Previews: PreviewProvider {
     static var previews: some View {
         HomeScreenView()
     }
 }
+
