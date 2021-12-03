d8ec9903fac2a31809bc8ce387d55eae9e9904dd
JakeS <jake72sullivan@gmail.com>
2021-12-03 12:54:29 -0500

    yeps

---
+++ Teacher App/Views/editGame.swift
@@ -0,0 +1,26 @@
+//
+//  editGame.swift
+//  Teacher App
+//
+//  Created by Jake S on 12/3/21.
+//
+
+import SwiftUI
+
+struct editGame: View {
+    var body: some View {
+        VStack {
+            CustomButton(name: "Class Management", width: 225, height: 85, color: .red, secondaryColor: .blue)
+            CustomButton(name: "Create Class", width: 125, height: 50, color: .red, secondaryColor: .blue)
+                .padding()
+            CustomLeaderboard(cname:"A Block", studentlist: ["math","science","literacy","Mr. Bailey's class","Sleep time"])
+        }
+
+    }
+}
+
+struct editGame_Previews: PreviewProvider {
+    static var previews: some View {
+        editGame()
+    }
+}
